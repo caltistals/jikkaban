42c43d3efe048102b9ba5063e770a459fd363ad5
caltistals <y.db.qp.y@gmail.com>
2023-03-23 22:57:52 +0900

    add: HelmetProviderを追加

--- src/providers/app.tsx
+++ src/providers/app.tsx
@@ -3,15 +3,18 @@ import { MantineProvider } from "@mantine/core";
 import { BrowserRouter as Router } from "react-router-dom";
 import FirebaseApp from "./FirebaseApp";
 import { NotificationsProvider } from "@mantine/notifications";
+import { HelmetProvider } from "react-helmet-async";
 
 export const AppProvider: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS>
-      <NotificationsProvider>
-        <FirebaseApp>
-          <Router>{children}</Router>
-        </FirebaseApp>
-      </NotificationsProvider>
-    </MantineProvider>
+    <HelmetProvider>
+      <MantineProvider withGlobalStyles withNormalizeCSS>
+        <NotificationsProvider>
+          <FirebaseApp>
+            <Router>{children}</Router>
+          </FirebaseApp>
+        </NotificationsProvider>
+      </MantineProvider>
+    </HelmetProvider>
   );
 };
